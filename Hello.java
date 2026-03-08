3c4439dfac2d8412e7af63056c99c6c89a75351d
Rairebsr <rainasusan220@gmail.com>
2026-03-08 23:05:15 +0530

    Save local changes before pull (Auto-sync)

--- Hello.java
+++ Hello.java
@@ -2,6 +2,10 @@ class Hello
 	{
 	public static void main(string args[])
 		{
+<<<<<<< HEAD
 		  system.out.println("Hello worl");
+=======
+		  system.out.println("Hello wo");
+>>>>>>> 7e3af11 (Save local changes before pull (Auto-sync))
 		}
 	}
